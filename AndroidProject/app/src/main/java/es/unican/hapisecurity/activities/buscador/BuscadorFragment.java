f86d64a0847b10036a28450353a23ab06cd5c97a
Mario Ingelmo Diana <mid292@alumnos.unican.es>
2023-04-25 13:58:23 +0200

    Anhadir buscador simple, faltan filtros y ordenación 25/04

--- AndroidProject/app/src/main/java/es/unican/hapisecurity/activities/buscador/BuscadorFragment.java
+++ AndroidProject/app/src/main/java/es/unican/hapisecurity/activities/buscador/BuscadorFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
+
 import androidx.fragment.app.Fragment;
 import com.google.android.material.navigation.NavigationView;
 import es.unican.hapisecurity.R;
@@ -23,6 +25,21 @@ public class BuscadorFragment extends Fragment {
         // Resaltar el ítem correspondiente en el menú lateral
         NavigationView navigationView = getActivity().findViewById(R.id.navigation_view);
         navigationView.setCheckedItem(selectedMenuIndex);
+
+        SearchView searchView = view.findViewById(R.id.svBuscador);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Realizar la búsqueda aquí
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+
+        });
 
         return view;
     }
