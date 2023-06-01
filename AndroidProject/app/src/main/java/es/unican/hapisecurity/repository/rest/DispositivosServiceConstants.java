a74d1df2e17cab84d81bf29391cc212fd2ff444c
Mario Ingelmo Diana <mid292@alumnos.unican.es>
2023-06-01 21:29:13 +0200

    Mas cosas para pruebas 01/06/2023

--- AndroidProject/app/src/main/java/es/unican/hapisecurity/repository/rest/DispositivosServiceConstants.java
+++ AndroidProject/app/src/main/java/es/unican/hapisecurity/repository/rest/DispositivosServiceConstants.java
@@ -9,7 +9,7 @@ public class DispositivosServiceConstants {
             "http://51.137.100.222:8080/";
 
     private static final String PRUEBAS_URL =
-            "https://github.com/MarioIngelmo/TFG-MarioIngelmoDiana/tree/b2579809d7c1f70cb6076890732fcf9c586a8382/StaticREST/1/";
+            "https://raw.githubusercontent.com/MarioIngelmo/TFG-MarioIngelmoDiana/develop/StaticREST/";
 
     private DispositivosServiceConstants() {}
 
