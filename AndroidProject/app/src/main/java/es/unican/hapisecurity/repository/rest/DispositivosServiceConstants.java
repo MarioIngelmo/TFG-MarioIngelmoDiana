c060abe4140c692e0fa9341d6d93eb29d850f7b1
Mario Ingelmo Diana <mid292@alumnos.unican.es>
2023-06-01 20:44:52 +0200

    Anhadir json estaticos para pruebas 01/06/2023

--- AndroidProject/app/src/main/java/es/unican/hapisecurity/repository/rest/DispositivosServiceConstants.java
+++ AndroidProject/app/src/main/java/es/unican/hapisecurity/repository/rest/DispositivosServiceConstants.java
@@ -8,12 +8,19 @@ public class DispositivosServiceConstants {
     private static final String SERVICIO_API_URL =
             "http://51.137.100.222:8080/";
 
+    private static final String PRUEBAS_URL =
+            "http://51.137.100.222:8080/";
+
     private DispositivosServiceConstants() {}
 
     private static String API_URL = SERVICIO_API_URL;
 
-    public static void setMinecoURL() {
+    public static void setApiUrl() {
         API_URL = SERVICIO_API_URL;
+    }
+
+    public static void setPruebasUrl() {
+        API_URL = PRUEBAS_URL;
     }
 
     public static final String getAPIURL() {
